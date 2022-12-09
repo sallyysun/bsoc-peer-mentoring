2d5dab44cf587a9e72ca9e36aebf5e169cce78ed
Sally Sun <sallyysun101@gmail.com>
2022-12-09 15:17:07 +1100

    basic

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./index.css";
 
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
+        <Header />
     </>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>RoadTrip Planner</h1>
+        <h4 className="text-light">By MJ, Rachel, Raiyan, Sally</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
