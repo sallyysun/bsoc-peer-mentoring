adc3b021177352e34cfd4134f98d5a799bb1ba25
Sally Sun <sallyysun101@gmail.com>
2022-12-09 17:50:15 +1100

    poo

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1>BSOC Peer Mentoring</h1>
-      <h4 className="text-light">By Sally and Oliver</h4>
+      <h4 className="text-light">By Sally and Oliver POO</h4>
     </div>
   );
 };
