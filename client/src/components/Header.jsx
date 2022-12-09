0f22683dc3b250fdde4b963bb19d383ea91118bf
Sally Sun <sallyysun101@gmail.com>
2022-12-09 15:17:31 +1100

    changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className="container">
       <div className="header">
-        <h1>RoadTrip Planner</h1>
-        <h4 className="text-light">By MJ, Rachel, Raiyan, Sally</h4>
+        <h1>BSOC Peer Mentoring</h1>
+        <h4 className="text-light">bleh</h4>
       </div>
     </div>
   );
