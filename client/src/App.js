ee26e354639a45fa96cf562857a979ee14769c31
Sally Sun <sallyysun101@gmail.com>
2022-12-09 15:28:45 +1100

    upload & css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./index.css";
 
-import Header from "./components/Header";
+import Header from "./components/header/Header";
+import Upload from "./components/Upload";
 
 const App = () => {
   return (
-    <>
+    <div className="container">
         <Header />
-    </>
+        <Upload />
+    </div>
   );
 }
 
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>BSOC Peer Mentoring</h1>
+      <h4 className="text-light">bleh</h4>
+    </div>
+  );
+};
+
+export default Header;
